cb3197317e64a48d59453b4c22d6ad2bf5814916
kosolapova <kosolapova97@mail.ru>
2018-11-10 22:51:38 +0300

    GC content counter fixed

--- RNA_DNA_classes.py
+++ RNA_DNA_classes.py
@@ -1,14 +1,14 @@
 class Rna(str):
     base_pairs = {'A': 'U', 'U': 'A',  'G': 'C', 'C': 'G', 'a': 'u', 'u': 'a',  'g': 'c', 'c': 'g'}
 
-    def __init__(self, sequence):
+    def __init__(self):
         self.sequence = sequence
         for base in self.sequence:
             if base not in self.base_pairs:
                 raise ValueError('RNA sequence can contain only A, U, G and C bases')
 
     def gc(self):
-        return (self.count('G') + self.count('C'))/len(self)
+        return (self.count('G')+self.count('g')+self.count('C')+self.count('c')) / len(self) * 100
 
     def reverse_complement(self):
         complement = ''
@@ -20,9 +20,9 @@ class Rna(str):
 class Dna(Rna):
     base_pairs = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G', 'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
 
-    def __init__(self, sequence):
+    def __init__(self):
         self.sequence = sequence
-        for base in sequence:
+        for base in self.sequence:
             if base not in self.base_pairs:
                 raise ValueError('DNA sequence can contain only A, T, G and C bases')
 
@@ -32,5 +32,3 @@ class Dna(Rna):
         for base in self:
             rna += dna_to_rna[base]
         return Rna(rna)
-
-
